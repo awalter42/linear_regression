0b20879fa4b4c2c45196bff00418d54a80afff19
Andrea <andrea.walter03@gmail.com>
2024-09-03 14:08:56 +0200

    made a class for the model

--- train.py
+++ train.py
@@ -1,96 +1,133 @@
+import matplotlib.pyplot as plt
 from statistics import mean
 import numpy as np
 
 
-def fillTab(file_name):
-	file = open(file_name, "r")
-	file.readline()
-	tab = []
-	for line in file:
-		splitted = line.split(",")
-		splitted[0] = int(splitted[0])
-		splitted[1] = int(splitted[1])
-		# splitted[1] = 1000
-		tab.append(splitted)
-	return (tab)
+def estimatePrice(mileage, model):
+	return (model.getTheta0() + (model.getTheta1() * mileage))
 
 
-def standardize(tab):
-	mile = [tab[i][0] for i in range(len(tab))]
-	price = [tab[i][1] for i in range(len(tab))]
-	mean_val = mean(mile)
+class Model:
 
-	variance = 0
-	for i in range(len(mile)):
-		variance += (mile[i] - mean_val)**2
-	variance /= len(mile)
+	def __init__(self):
+		self.theta0 = 9000
+		self.theta1 = -0.1
+		self.learningRate = 0.01
+		self.nb_itter = 0
 
-	stan_dev = variance**(1/2)
+		self.file = input('File with dataset: ')
+		try:
+			self.originalTab = self._fillTab()
+		except:
+			print("There has been a problem with the file you sent")
+			return -1
+		self.tab = self._standardize()
 
-	for i in range(len(mile)):
-		mile[i] = (mile[i] - mean_val)/stan_dev
-
-	new_tab = [[mile[i], price[i]] for i in range(len(mile))]
-	return new_tab, mean_val, stan_dev
-
-
-def destandardize(est, mean_val, stan_dev):
-	return est * stan_dev + mean_val
+		self.loopTrain(1000)
 
 
-def estimatePrice(mileage):
-	return (theta0 + (theta1 * mileage))
+	def __str__(self):
+		return(f"""
+file = {self.file}
+theta0 = {self.theta0}
+theta1 = {self.theta1}
+learning rate = {self.learningRate}
+iteration = {self.nb_itter}
+""")
 
 
-def meanSquareError(tab):
-	mse = sum((estimatePrice(tab[i][0]) - tab[i][1]) ** 2 for i in range (len(tab)))
-	mse /= 2 * len(tab)
-	print(mse)
+	def getNbItter(self):
+		return (self.nb_itter)
+
+	def getFile(self):
+		return (self.file)
+
+	def getTheta0(self):
+		return (self.theta0)
 
 
-def training(tab):
-	global theta0
-	global theta1
-	global nb_itter
-	global nb_values
-
-	nb_itter += 1
+	def getTheta1(self):
+		return (self.theta1)
 
 
-	tmpTheta0 = sum(estimatePrice(tab[i][0]) - tab[i][1] for i in range(len(tab)))
-	tmpTheta1 = sum((estimatePrice(tab[i][0]) - tab[i][1]) * tab[i][0] for i in range(len(tab)))
-	nb_values += len(tab)
+	def _fillTab(self):
+		file = open(self.file, "r")
+		file.readline()
+		tab = []
+		for line in file:
+			splitted = line.split(",")
+			splitted[0] = int(splitted[0])
+			splitted[1] = int(splitted[1])
+			# splitted[1] = 1000
+			tab.append(splitted)
+		return (tab)
 
 
-	learningRate = 0.01
-	tmpTheta0 /= len(tab)	
-	tmpTheta1 /= len(tab)
+	def _standardize(self):
+		mile = [self.originalTab[i][0] for i in range(len(self.originalTab))]
+		price = [self.originalTab[i][1] for i in range(len(self.originalTab))]
+		mean_val = mean(mile)
 
-	if nb_itter < 10:
-		print (tmpTheta0, tmpTheta1)
+		variance = 0
+		for i in range(len(mile)):
+			variance += (mile[i] - mean_val)**2
+		variance /= len(mile)
+		stan_dev = variance**(1/2)
+		for i in range(len(mile)):
+			mile[i] = (mile[i] - mean_val)/stan_dev
 
-	tmpTheta0 *= learningRate
-	tmpTheta1 *= learningRate
+		print(mean_val, stan_dev)
 
-	theta0 -= tmpTheta0
-	theta1 -= tmpTheta1
-
-	# theta0 = ((theta0 * (nb_values - len(tab))) + (tmpTheta0 * len(tab))) / nb_values
-	# theta1 = ((theta1 * (nb_values - len(tab))) + (tmpTheta1 * len(tab))) / nb_values
+		new_tab = [[mile[i], price[i]] for i in range(len(mile))]
+		return new_tab
 
 
-if __name__ == '__main__':
-	theta0 = 0
-	theta1 = 0
-	nb_values = 0
-	nb_itter = 0
+	def meanSquareError(self):
+		mse = sum((estimatePrice(self.tab[i][0], self) - self.tab[i][1]) ** 2 for i in range (len(self.tab)))
+		mse /= 2 * len(self.tab)
+		return (mse)
 
-	tab = fillTab("data.csv")
-	# tab.sort()
-	# tab[0][1] = 1001
-	stan_tab, mean_val, stan_dev = standardize(tab)
 
-	for _ in range (1000):
-		training(stan_tab)
+	def loopTrain(self, iterations):
+		for _ in range(iterations):
+			self._training()
 
-	print (theta0, theta1)
+
+	def _training(self):
+		tmpTheta0 = sum(estimatePrice(self.tab[i][0], self) - self.tab[i][1] for i in range(len(self.tab)))
+		tmpTheta1 = sum((estimatePrice(self.tab[i][0], self) - self.tab[i][1]) * self.tab[i][0] for i in range(len(self.tab)))
+
+		tmpTheta0 /= len(self.tab)	
+		tmpTheta1 /= len(self.tab)
+
+		# if self.nb_itter % 10 == 0:
+		# 	print (tmpTheta0, tmpTheta1)
+
+		tmpTheta0 *= self.learningRate
+		tmpTheta1 *= self.learningRate
+
+		self.theta0 -= tmpTheta0
+		self.theta1 -= tmpTheta1
+
+		self.nb_itter += 1
+
+		# theta0 = ((theta0 * (nb_values - len(tab))) + (tmpTheta0 * len(tab))) / nb_values
+		# theta1 = ((theta1 * (nb_values - len(tab))) + (tmpTheta1 * len(tab))) / nb_values
+
+
+	def showData(self, show_func):
+		fig, ax = plt.subplots()
+
+		mileage = []
+		price = []
+		for row in self.originalTab:
+			mileage.append(row[0])
+			price.append(row[1])
+
+		ma = np.arange(0, 250000, 1000)
+		func = estimatePrice(ma, self)
+
+		ax.scatter(mileage, price)
+		if show_func is True:
+			ax.plot(ma, func)
+		plt.show()
